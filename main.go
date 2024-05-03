f25975bdcb61ddf318507a0c04d7c696790e72ee
ttl256 <64251319+ttl256@users.noreply.github.com>
2024-05-03 16:13:47 +0200

    Add json handling

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
 	const tcpAddr = "0.0.0.0:8080"
 
+	cfg := &apiConfig{
+		fileserverHits: 0,
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", ReadinessHandler)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
+	mux.HandleFunc("GET /api/healthz", readinessHandler)
+	mux.HandleFunc("GET /api/reset", cfg.metricsResetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 
 	s := http.Server{ //nolint: gosec // let me be
 		Addr:    tcpAddr,
@@ -23,10 +36,118 @@ func main() {
 	}
 }
 
-func ReadinessHandler(w http.ResponseWriter, _ *http.Request) {
+func readinessHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnValues struct {
+		Body string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{Body: ""}
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("error decoding json: %s", err)
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("could not decode parameters: %s", err))
+		return
+	}
+
+	const maxLen = 140
+	if utf8.RuneCountInString(params.Body) > maxLen {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	profanes := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+
+	respondWithJSON(w, http.StatusOK, returnValues{Body: filterWords(params.Body, profanes, "****")})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	if code >= http.StatusInternalServerError {
+		log.Printf("responding with 5xx error: %s", msg)
+	}
+	respondWithJSON(w, code, errorResponse{Error: msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("error encoding JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	_, err = w.Write(data)
+	if err != nil {
+		log.Printf("error writing JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func filterWords(s string, w map[string]struct{}, sub string) string {
+	words := strings.Fields(s)
+	for i, word := range words {
+		if _, ok := w[strings.ToLower(word)]; ok {
+			words[i] = sub
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		log.Printf("Hits: %d", cfg.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
+}
+
+const templ = `<html>
+
+<body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>`
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf(templ, cfg.fileserverHits)))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
